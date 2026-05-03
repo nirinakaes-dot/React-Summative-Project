b9b7db3f439eec0951bf70608a42713f95eeb99a
Nina Adora <nirinakaes@gmail.com>
2026-05-03 21:43:36 +0300

    style: add MyContacts component for improved user engagement and contact options

---
+++ React Summative Project/src/Additions/Contact.jsx
@@ -0,0 +1,17 @@
+
+export default function MyContacts(){
+
+
+return(
+    <footer>
+        <p>Interested In My Work or Have Any Inquiries??</p>
+        <h2>Get In Touch:</h2>
+        <a className="bg-sky-700 px-7 m-3 text-white rounded-xl" href="#">Gmail</a>
+        <a className="bg-sky-700 px-7  rounded-xl m-3 text-white" href="#">Instagram</a>
+        <a className="bg-sky-700 rounded-xl m-3 px-7 text-white" href="#">LinkedIn</a>
+
+    </footer>
+)
+
+
+}
--- React Summative Project/src/App.jsx
+++ React Summative Project/src/App.jsx
@@ -4,6 +4,7 @@ import ProjectForm from './Additions/ProjectForm'
 import BarSearch from './Additions/BarSearch'
 import CardProjects from './Additions/CardProjects'
 import { useState } from 'react'
+import MyContacts from './Additions/Contact'
 
 
 export default function App(){
@@ -22,13 +23,13 @@ export default function App(){
     <NavBar />
     <div className='grid gap-6 px-6 py-8 md:grid-cols-2'>
 
-      <div className='rounded-3xl border border-sky-200 bg-sky-50/80 p-6 shadow-sm'>
+      <div className='rounded-3xl border-4 border-white-200 bg-sky-50/80 p-6 shadow-sm'>
     
     <ProjectForm onSubmission={Submission} />
 
       </div>
 
-    <div className='overflow-auto rounded-3xl border border-sky-200 bg-white/75 p-6 shadow-sm backdrop-blur-sm'>
+    <div className='overflow-auto rounded-3xl border-4 border-white-200 bg-white/75 p-6 shadow-sm backdrop-blur-sm'>
 
     <BarSearch onSearch={setQuery} />
     {Filtered.length ===0 ? (<p className='text-white font-bold justify-self-center'>No Projects Added</p>)
@@ -42,6 +43,7 @@ export default function App(){
     
       
     </div>
+    <MyContacts />
 
     
     
