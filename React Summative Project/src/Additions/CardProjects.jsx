92c03735154efb2cb6dccd70856d7464682098e9
Nina Adora <nirinakaes@gmail.com>
2026-05-03 14:34:57 +0300

    Enhance CardProjects component structure for better readability

--- React Summative Project/src/Additions/CardProjects.jsx
+++ React Summative Project/src/Additions/CardProjects.jsx
@@ -2,5 +2,19 @@
 
 export default function CardProjects(){
 
-    
+    return(
+        <>
+        <section>
+            <h3>{Project.ProjectName}</h3>
+
+            <p>{Project.ProjectDescription}</p>
+
+
+
+
+        </section>
+        
+        
+        </>
+    )
 }
