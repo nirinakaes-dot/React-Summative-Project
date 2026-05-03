8110ccf829504c18962b71ad53f2ff7bbbbf88fd
Nina Adora <nirinakaes@gmail.com>
2026-05-03 12:53:52 +0300

    Add ProjectForm component with form fields for project name and description; create CardProjects component

--- React Summative Project/src/Additions/ProjectForm.jsx
+++ React Summative Project/src/Additions/ProjectForm.jsx
@@ -1,1 +1,37 @@
+import { useState } from "react"
 
+export default function ProjectForm(){
+    const [ProjectName, setProjectName]= useState('')
+    const [ProjectDescription, setProjectDescription]= useState('')
+
+
+    return(
+        <>
+        <section>
+            
+             <form>
+                <h2>Add Project</h2>
+
+                <label>Project Name</label>
+                <input type="text"></input>
+
+                <label>Project Description</label>
+                <input type="typearea"></input>
+                
+                <button>Add Project</button>
+
+            </form>
+
+
+
+
+        </section>
+           
+        
+        
+        
+        
+        
+        </>
+    )
+}
