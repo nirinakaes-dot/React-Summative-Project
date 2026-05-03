8a0e1bf0ba5944547b5dfb3f2dc122351ce37954
Nina Adora <nirinakaes@gmail.com>
2026-05-03 21:47:29 +0300

    style: enhance link styles in MyContacts component for improved user interaction

--- React Summative Project/src/Additions/Contact.jsx
+++ React Summative Project/src/Additions/Contact.jsx
@@ -6,9 +6,9 @@ return(
     <footer>
         <p>Interested In My Work or Have Any Inquiries??</p>
         <h2>Get In Touch:</h2>
-        <a className="bg-sky-700 px-7 m-3 text-white rounded-xl" href="#">Gmail</a>
-        <a className="bg-sky-700 px-7  rounded-xl m-3 text-white" href="#">Instagram</a>
-        <a className="bg-sky-700 rounded-xl m-3 px-7 text-white" href="#">LinkedIn</a>
+        <a className="bg-sky-700 hover:bg-sky-500 px-7 m-3 text-white rounded-xl" href="#">Gmail</a>
+        <a className="bg-sky-700 px-7  hover:bg-sky-500 rounded-xl m-3 text-white" href="#">Instagram</a>
+        <a className="bg-sky-700 rounded-xl hover:bg-sky-500 m-3 px-7 text-white" href="#">LinkedIn</a>
 
     </footer>
 )
