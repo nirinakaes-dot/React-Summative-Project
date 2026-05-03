d8216bd40849235502106dd2e590776cdc6a650b
Nina Adora <nirinakaes@gmail.com>
2026-05-03 12:53:11 +0300

    Add ProjectForm, BarSearch, and CardProjects components to App

--- React Summative Project/src/App.jsx
+++ React Summative Project/src/App.jsx
@@ -2,12 +2,18 @@ import { useState } from 'react'
 import './App.css'
 import './Additions/NavBar'
 import NavBar from './Additions/NavBar'
+import ProjectForm from './Additions/ProjectForm'
+import BarSearch from './Additions/BarSearch'
+import CardProjects from './Additions/CardProjects'
 
 
 export default function App(){
   return(
     <>
     <NavBar />
+    <ProjectForm />
+    <BarSearch />
+    <CardProjects />
     
     
     </>
